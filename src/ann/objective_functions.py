58f9f204456c38fcebc87ab2df5d65eb32068beb
manoj-vjkmr <manojharish565@gmail.com>
2026-03-07 21:15:47 +0530

    more fixes

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -11,24 +11,26 @@ def cross_entropy_loss(logits, y):
     loss= -np.log(probs[range(m), y])
     return np.mean(loss)
 
-def cross_entropy_grad(y, pred):
-    batch_size = pred.shape[0]
-    y_idx = y.astype(int).flatten()
-    
-    grad = pred.copy()
-    grad[np.arange(batch_size), y_idx] -= 1
-    
-    return grad / batch_size
-
 def mse_loss(pred, y):
     if y.ndim == 1:
         y = np.eye(pred.shape[1])[y]
     return np.mean((pred - y) ** 2)
 
 def mse_grad(y, pred):
+    batch_size = pred.shape[0]
+    y_int = y.astype(int).flatten()
     y_one_hot = np.zeros_like(pred)
-    y_idx = y.astype(int).flatten()
+    y_one_hot[np.arange(batch_size), y_int] = 1
     
-    y_one_hot[np.arange(len(y_idx)), y_idx] = 1
+    return 2 * (pred - y_one_hot) / batch_size
+
+def cross_entropy_grad(y, pred):
+    batch_size = pred.shape[0]
+    y_int = y.astype(int).flatten()
     
-    return 2 * (pred - y_one_hot) / y.size
+    probs = pred
+    
+    grad = probs.copy()
+    grad[np.arange(batch_size), y_int] -= 1
+    
+    return grad / batch_size
